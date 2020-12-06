58eb167431442c6f9266ff10041fff6b7ef3369e
Michael Viveros <michaelviveros@gmail.com>
2020-12-06 14:18:27 -0500

    add binary numbers method to D5

--- 2020/5/binary_seats.rb
+++ 2020/5/binary_seats.rb
@@ -19,9 +19,33 @@ def binary_partition(seat)
   row * 8 + column
 end
 
+# @param seat [String] encoded seat number like 'FBFFBBFRLR'
+# @return [Integer] decoded seat number
+# decodes row by converting encoded row to binary number
+# does same thing for column
+def binary_partition2(seat)
+  row_binary = seat[0..6].gsub('F', '0').gsub('B', '1')
+  row_decimal = row_binary.to_i(2)
+  column_binary = seat[7..9].gsub('L', '0').gsub('R', '1')
+  column_decimal = column_binary.to_i(2)
+  row_decimal * 8 + column_decimal
+end
+
 f = File.new('input.txt')
-seats = f.readlines.map { |l| binary_partition(l) }
+lines = f.readlines
+seats = lines.map { |l| binary_partition(l) }
 puts seats.max
 seats.sort!
-seat, _index = seats.each_with_index.find { |s, i| seats[i + 1] != s + 1 }
+seat, _index = seats.each_with_index.find do |s, i|
+  seats[i + 1] != s + 1
+end
 puts seat + 1
+
+puts 'using binary numbers:'
+seats2 = lines.map { |l| binary_partition2(l) }
+puts seats2.max
+seats2.sort!
+seat2, _index2 = seats2.each_with_index.find do |s, i|
+  seats2[i + 1] != s + 1
+end
+puts seat2 + 1
